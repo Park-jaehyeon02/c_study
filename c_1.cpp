224b62e14d430987b64000ee0543c06b7dcd66b0
현 재 <1234qkr@gmail.com>
2021-12-05 22:07:39 +0900

    fix

--- c_1.cpp
+++ c_1.cpp
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
-char a;
-short b;
-int c;
-long d;
-long long int e;
-unsigned int f;
-float g;
-double h;
-long double i;
+void main()
+{
+	char a;
+	short b;
+	int c;
+	long d;
+	long long int e;
+	unsigned int f;
+	float g;
+	double h;
+	long double i;
+	return 0;
+}
