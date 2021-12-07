1017cdb4a0229c8d9db60096300485c0234bbb2f
현 재 <1234qkr@gmail.com>
2021-12-07 14:49:07 +0900

    fix

--- c_1.cpp
+++ c_1.cpp
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-void main()
+int main(void)
 {
-	char a;
-	short b;
-	int c;
-	long d;
-	long long int e;
-	unsigned int f;
+	char a;	//1byte
+	short b;	//2byte
+	int c;	//4byte
+	long d;	//4byte
+	long long int e;	//8byte
+	unsigned int f;	//4byte
 	float g;
 	double h;
 	long double i;
-	return 0;
+	printf("%d", sizeof(f));
 }
--- c_2.cpp
+++ c_2.cpp
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-int main(void)
+int main2(void)
 {
 	char ch = 0;
 	ch = getchar();
