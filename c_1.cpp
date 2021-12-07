78459cbc1591e723f18e12d4e06048654fbb5def
현 재 <1234qkr@gmail.com>
2021-12-07 19:35:17 +0900

    fix

--- c_1.cpp
+++ c_1.cpp
@@ -8,8 +8,7 @@ int main(void)
 	long d;	//4byte
 	long long int e;	//8byte
 	unsigned int f;	//4byte
-	float g;
-	double h;
-	long double i;
-	printf("%d", sizeof(f));
+	float g;	//4byte
+	double h;	//8byte
+	printf("%f %d", i,sizeof(i));
 }
