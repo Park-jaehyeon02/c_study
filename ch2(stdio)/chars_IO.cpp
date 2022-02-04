0ed1a1a1ee14284bb2616939bf4495a5605ca2e2
현 재 <1234qkr@gmail.com>
2022-02-04 20:28:30 +0900

    add

--- ch2(stdio)/chars_IO.cpp
+++ ch2(stdio)/chars_IO.cpp
@@ -15,5 +15,6 @@ int main(void)
 	printf("당신의 이름은 ");
 	puts(szName);
 	puts("입니다.");
+
 	return 0;
 }
--- ch2(stdio)/summary.cpp
+++ ch2(stdio)/summary.cpp
@@ -2,7 +2,20 @@
 
 int main(void)
 {
-	printf("%c %d %o %u %x %X %e %E %f %g %p %s");
+	/*
+	c : character. ASCII 출력
+	d : Decimal. 부호가 있는 10진수
+	o : Octal. 8진수
+	u : Unsigned int. 부호없는 10진수
+	x : Hexa. 16진수
+	e : Exponent. 지수형 소수
+	f : Float. 10진형 소수
+	g : 지수형 소수(%e)나 10진형 소수(%f) 출력
+	p : Pointer. 16진수 주소 출력
+	s : String. 인수가 가리키는 메모리의 내용을 문자열로 출력
+	*/
+
+	printf("%c %d %o %u %x %X %e %E %f %g %p %s"); 
 	printf("\a \b \f \n \r \t \v \\ \? \' \" \ooo \xhh");
 	// '와 ?는 이제 그냥 써도 인식이 되도록 바뀜
 	printf("' ?");
