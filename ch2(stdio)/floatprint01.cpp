66c4d60d76c8871b5ef4cf612f2089e0861d4913
현 재 <1234qkr@gmail.com>
2022-02-05 18:54:55 +0900

    add

---
+++ ch2(stdio)/floatprint01.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%d\n", sizeof(123.45F));
+	printf("%d\n", sizeof(123.45));
+	
+	printf("%f\n", 123.45F); //float
+
+	printf("%f\n", 123.45); //double
+	printf("%lf\n", 123.45); //long double
+	return 0;
+}
