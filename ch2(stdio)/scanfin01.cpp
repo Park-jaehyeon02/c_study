34548c6ea1a5c4f9176f0a35ee0e97d3c2176a7a
현 재 <1234qkr@gmail.com>
2022-02-20 22:50:19 +0900

    add

---
+++ ch2(stdio)/scanfin01.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int nAge = 0;
+
+	printf("나이를 입력하세요 : ");
+
+	scanf("%d", &nAge);
+	
+	// Need to turn off SDL Check
+
+	printf("당신의 나이는 %d세 입니다.\n", nAge);
+	return 0;
+}
